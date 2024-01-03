970bf1d1d738681df9b445206061dfa12e1a8c03
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-01-03 14:48:07 -0500

    Create fasta_dict.sh

---
+++ gatk/fasta_dict.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=p_ccib_1
+#SBATCH --account=general
+#SBATCH --exclude=gpuc001,gpuc002
+#SBATCH --job-name=dict
+#SBATCH --output=/projects/f_geneva_1/alyssa/sagrei/slurmout/slurm-%j-%x.out
+#SBATCH --mem=60G
+#SBATCH -n 15
+#SBATCH -N 1
+#SBATCH --time=1-00:00:00
+#SBATCH --requeue
+#SBATCH --mail-user=av795@rutgers.edu
+#SBATCH --mail-type=FAIL
+
+echo "######################## modules"
+
+module purge
+module use /projects/community/modulefiles/
+module load GATK/4.2.2.0-yc759
+module load bedtools2/2.25.0
+module load gcc/7.3.0-gc56
+
+echo "create file"
+gatk CreateSequenceDictionary -R AnoSag2.1.fa
