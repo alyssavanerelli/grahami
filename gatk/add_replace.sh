988f6d69bd9394cc67994b23f8ca5245567a3c56
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-01-03 14:58:34 -0500

    Update add_replace.sh

--- gatk/add_replace.sh
+++ gatk/add_replace.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --partition=p_geneva_1
+#SBATCH --partition=cmain
 #SBATCH --exclude=gpuc001,gpuc002
 #SBATCH --job-name=addorreplace
-#SBATCH --output=/projects/f_geneva_1/alyssa/sagrei/add_replace/slurmout/slurm-%j-%x.out
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/gatk/slurmout/slurm-%j-%x.out
 #SBATCH --mem=50G
 #SBATCH -n 10
 #SBATCH -N 1
@@ -25,8 +25,8 @@ BAM=$2
 PU=$3
 READ=$4
 CORE=$5
-OUTDIR="/projects/f_geneva_1/alyssa/sagrei/add_replace"
-BAM_DIR="/projects/f_geneva_1/alyssa/sagrei/bam"
+OUTDIR="/projects/f_geneva_1/alyssa/grahami/gatk/add_replace"
+BAM_DIR="/projects/f_geneva_1/alyssa/grahami/gatk/bam"
 
 echo "run add or replace groups"
 gatk AddOrReplaceReadGroups \
