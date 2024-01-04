b45ea5f23b414f5152defc8fc9578849b9a70449
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-01-04 13:13:53 -0500

    Update mark_dup.sh

--- gatk/mark_dup.sh
+++ gatk/mark_dup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --partition=p_geneva_1
+#SBATCH --partition=cmain
 #SBATCH --exclude=gpuc001,gpuc002
 #SBATCH --job-name=MarkDup
-#SBATCH --output=/projects/f_geneva_1/alyssa/sagrei/mark_duplicates/slurmout/slurm-%j-%x.out
-#SBATCH --mem=240G
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/gatk/slurmout/slurm-%j-%x.out
+#SBATCH --mem=180G
 #SBATCH -n 10
 #SBATCH -N 1
 #SBATCH --time=3-00:00:00
@@ -21,27 +21,20 @@ module load GATK/4.2.2.0-yc759
 
 echo ""
 echo "load variables"
-SAMPLE=$1
-INDIR="/projects/f_geneva_1/alyssa/sagrei/add_replace"
-OUTDIR="/projects/f_geneva_1/alyssa/sagrei/mark_duplicates"
-#OUTDIR="/scratch/av795"
+#SAMPLE=$1
+SAMPLE='DTG-SG-149'
+INDIR="/projects/f_geneva_1/alyssa/grahami/gatk/add_replace"
+OUTDIR="/projects/f_geneva_1/alyssa/grahami/gatk/mark_duplicates"
 
 
 echo ""
 echo "run mark duplicates"
 gatk MarkDuplicates \
 --java-options "-Xms200G -Xmx240g" \
--I ${INDIR}/${SAMPLE}_Lane1.addGP.bam \
--I ${INDIR}/${SAMPLE}_Lane2.addGP.bam \
--I ${INDIR}/${SAMPLE}_Lane3.addGP.bam \
--I ${INDIR}/${SAMPLE}_Lane4.addGP.bam \
+-I ${INDIR}/${SAMPLE}.addGP.bam \
 -O ${OUTDIR}/${SAMPLE}.marked.bam \
 -M ${OUTDIR}/${SAMPLE}.metrics.txt \
 --REMOVE_DUPLICATES false --ASSUME_SORTED true --CREATE_INDEX true
-
-
-#-I ${INDIR}/${SAMPLE}_Lane3.addGP.bam \
-#-I ${INDIR}/${SAMPLE}_Lane4.addGP.bam \
 
 echo ""
 echo "done"
