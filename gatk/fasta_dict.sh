ab94b52392d27b5cd5b2ad33eab92718bcbae329
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-01-04 16:10:25 -0500

    Update fasta_dict.sh

--- gatk/fasta_dict.sh
+++ gatk/fasta_dict.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --partition=p_ccib_1
+#SBATCH --partition=cmain
 #SBATCH --account=general
 #SBATCH --exclude=gpuc001,gpuc002
 #SBATCH --job-name=dict
-#SBATCH --output=/projects/f_geneva_1/alyssa/sagrei/slurmout/slurm-%j-%x.out
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/gatk/slurmout/slurm-%j-%x.out
 #SBATCH --mem=60G
 #SBATCH -n 15
 #SBATCH -N 1
@@ -21,4 +21,7 @@ module load bedtools2/2.25.0
 module load gcc/7.3.0-gc56
 
 echo "create file"
-gatk CreateSequenceDictionary -R AnoSag2.1.fa
+gatk CreateSequenceDictionary -R /projects/f_geneva_1/alyssa/grahami/AnoGra1.1.fa
+
+echo ""
+echo "done"
