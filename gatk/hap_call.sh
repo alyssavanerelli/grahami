2ecf26d6442a81639af55a008cac5c882f1cdf52
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-01-05 14:31:37 -0500

    Update hap_call.sh

--- gatk/hap_call.sh
+++ gatk/hap_call.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=p_ccib_1
 #SBATCH --exclude=gpuc001,gpuc002
 #SBATCH --job-name=gatk_HC
-#SBATCH --output=/projects/f_geneva_1/alyssa/sagrei/haplotype_caller/slurmout/slurm-%j-%x.out
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/gatk/slurmout/slurm-%j-%x.out
 #SBATCH --mem=75G
 #SBATCH -n 2
 #SBATCH -N 1
@@ -22,72 +22,24 @@ module load samtools
 
 echo ""
 echo "load variables"
-SAMPLE=$1
-INDIR="/projects/f_geneva_1/alyssa/sagrei/mark_duplicates"
-GEN_DIR="/projects/f_geneva_1/alyssa/sagrei/genome"
-OUTDIR="/projects/f_geneva_1/alyssa/sagrei/haplotype_caller"
-SCRATCH_DIR="/scratch/av795"
-
+#SAMPLE=$1
+SAMPLE='DTG-SG-149'
+INDIR="/projects/f_geneva_1/alyssa/grahami/gatk/mark_duplicates"
+GEN_DIR="/projects/f_geneva_1/alyssa/grahami"
+OUTDIR="/projects/f_geneva_1/alyssa/grahami/gatk/haplotype_caller"
+BASE_DIR='/projects/f_geneva_1/alyssa/grahami/gatk'
 
 echo ""
 echo "run haplotype caller"
 gatk --java-options "-Xms75G -Xmx75g -XX:ParallelGCThreads=2" HaplotypeCaller --native-pair-hmm-threads 2 \
 -I ${INDIR}/${SAMPLE}.marked.bam \
 -O ${OUTDIR}/${SAMPLE}.g.vcf.gz \
--R ${GEN_DIR}/AnoSag2.1.fa \
--ERC GVCF \
---max-reads-per-alignment-start 0 \
--RF NotDuplicateReadFilter
-
-
-echo ""
-echo "done"
-
-
-
-#!/bin/bash
-#SBATCH --partition=p_ccib_1
-#SBATCH --exclude=gpuc001,gpuc002
-#SBATCH --job-name=gatk_HC_split_1
-#SBATCH --mem=50G
-#SBATCH -n 2
-#SBATCH -N 1
-#SBATCH --time=5-00:00:00
-#SBATCH --requeue
-#SBATCH --mail-user=chf29@scarletmail.rutgers.edu
-#SBATCH --mail-type=FAIL,END
-
-
-echo "load modules"
-module purge
-module use /projects/community/modulefiles/
-module load java
-module load GATK/4.2.2.0-yc759
-module load samtools
-
-
-echo ""
-echo "load variables"
-SAMPLE=$1
-INDIR="/projects/f_geneva_1/chfal/distichus/gatk_pipeline/haplotype_caller"
-GEN_DIR="/projects/f_geneva_1/chfal/distichus/gatk_pipeline/haplotype_caller"
-OUTDIR="/projects/f_geneva_1/chfal/distichus/gatk_pipeline/haplotype_caller"
-
-echo ""
-echo "run haplotype caller"
-gatk --java-options "-Xms50G -Xmx50g -XX:ParallelGCThreads=2" HaplotypeCaller --native-pair-hmm-threads 2 \
--I ${INDIR}/${SAMPLE}.marked.bam \
--O ${OUTDIR}/${SAMPLE}.1.g.vcf.gz \
--R ${GEN_DIR}/AnoDis1.0.fasta \
+-R ${GEN_DIR}/AnoGra1.1.fa \
 -ERC BP_RESOLUTION \
 --output-mode EMIT_ALL_CONFIDENT_SITES \
 --max-reads-per-alignment-start 0 \
 -RF NotDuplicateReadFilter \
--L ${GEN_DIR}/scaffold_1.list
-
-
-#--exclude=halc068
-#--exclude=gpuc001,gpuc002
+-L ${BASE_DIR}/scaffold_1.list
 
 echo ""
 echo "done"
