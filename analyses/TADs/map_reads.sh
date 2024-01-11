51c95cc0a28e5a71455cb942db813ea274cce19c
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-01-11 12:18:53 -0500

    Update map_reads.sh

--- analyses/TADs/map_reads.sh
+++ analyses/TADs/map_reads.sh
@@ -1,17 +1,44 @@
-# map the reads, each mate individually using
-# for example bwa
-#
-# bwa mem mapping options:
-#       -A INT        score for a sequence match, which scales options -TdBOELU unless overridden [1]
-#       -B INT        penalty for a mismatch [4]
-#       -O INT[,INT]  gap open penalties for deletions and insertions [6,6]
-#       -E INT[,INT]  gap extension penalty; a gap of size k cost '{-O} + {-E}*k' [1,1] # this is set very high to avoid gaps
-#                                  at restriction sites. Setting the gap extension penalty high, produces better results as
-#                                  the sequences left and right of a restriction site are mapped independently.
-#       -L INT[,INT]  penalty for 5'- and 3'-end clipping [5,5] # this is set to no penalty.
+#!/bin/bash
+#SBATCH --partition=cmain
+#SBATCH --exclude=gpuc001,gpuc002
+#SBATCH --job-name=map_hic_reads
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/tads/slurmout/slurm-%j-%x.out
+#SBATCH --mem=100G
+#SBATCH -n 15
+#SBATCH -N 1
+#SBATCH --time=3-00:00:00
+#SBATCH --requeue
+#SBATCH --mail-user=av795@rutgers.edu
+#SBATCH --mail-type=FAIL
 
-$ bwa mem -A1 -B4  -E50 -L0  index_path \
-    mate_R1.fastq.gz 2>>mate_R1.log | samtools view -Shb - > mate_R1.bam
+echo "load any Amarel modules that script requires"
+module purge
+module load java 
+module load samtools
+module load bwa
 
-$ bwa mem -A1 -B4  -E50 -L0  index_path \
-    mate_R2.fastq.gz 2>>mate_R2.log | samtools view -Shb - > mate_R2.bam
+NAME=$1
+READS_OUTDIR="/projects/f_geneva_1/alyssa/grahami/juicerdir/AnoGra/fastq"
+BAM_OUTDIR="/projects/f_geneva_1/alyssa/grahami/tads/mapped_reads"
+
+echo ""
+echo "##################### index and align with BWA"
+# bwa index /projects/f_geneva_1/alyssa/grahami/AnoGra1.1.fa
+
+echo ""
+echo "R1 files"
+bwa mem -A1 -B4  -E50 -L0 -t 10 /projects/f_geneva_1/alyssa/grahami/AnoGra1.1.fa \
+${READS_OUTDIR}/${NAME}_R1_001.fastq.gz 2>>${BAM_OUTDIR}/${NAME}_R1.log | samtools view -Shb - > ${BAM_OUTDIR}/${NAME}_R1.bam
+
+echo ""
+echo "R2 files"
+bwa mem -A1 -B4  -E50 -L0 -t 10 /projects/f_geneva_1/alyssa/grahami/AnoGra1.1.fa \
+${READS_OUTDIR}/${NAME}_R2_001.fastq.gz 2>>${BAM_OUTDIR}/${NAME}_R2.log | samtools view -Shb - > ${BAM_OUTDIR}/${NAME}_R2.bam
+
+echo ""
+echo "##################### index all bam files"
+samtools index -b ${BAM_OUTDIR}/${NAME}_R1.bam
+samtools index -b ${BAM_OUTDIR}/${NAME}_R2.bam
+
+echo ""
+echo "done"
