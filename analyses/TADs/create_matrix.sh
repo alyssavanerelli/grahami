7e1222f5615d3a458ac0fd6ad668a20677173889
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-01-11 14:43:11 -0500

    Update create_matrix.sh

--- analyses/TADs/create_matrix.sh
+++ analyses/TADs/create_matrix.sh
@@ -1,10 +1,34 @@
-hicBuildMatrix --samFiles mate_R1.bam mate_R2.bam \
-                 --binSize 10000 \
-                 --restrictionSequence GATC \
-                 --danglingSequence GATC \
-                 --restrictionCutFile cut_sites.bed \
-                 --threads 4 \
-                 --inputBufferSize 100000 \
-                 --outBam hic.bam \
-                 -o hic_matrix.h5 \
-                 --QCfolder ./hicQC
+#!/bin/bash
+#SBATCH --partition=cmain
+#SBATCH --exclude=gpuc001,gpuc002
+#SBATCH --job-name=create_matrix
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/tads/slurmout/slurm-%j-%x.out
+#SBATCH --mem=100G
+#SBATCH -n 15
+#SBATCH -N 1
+#SBATCH --time=3-00:00:00
+#SBATCH --requeue
+#SBATCH --mail-user=av795@rutgers.edu
+#SBATCH --mail-type=FAIL
+
+echo "load conda environment"
+eval "$(conda shell.bash hook)"
+conda activate hicexplorer
+
+NAME=$1
+BAM_OUTDIR="/projects/f_geneva_1/alyssa/grahami/tads/mapped_reads"
+OUTDIR='/projects/f_geneva_1/alyssa/grahami/tads/matrix'
+
+hicBuildMatrix --samFiles ${BAM_OUTDIR}/${NAME}_R1.bam ${BAM_OUTDIR}/${NAME}_R2.bam \
+--binSize 10000 \
+--restrictionSequence GATC \
+--danglingSequence GATC \
+--restrictionCutFile cut_sites.bed \
+--threads 4 \
+--inputBufferSize 100000 \
+--outBam ${OUTDIR}/${NAME}_hic.bam \
+-o ${OUTDIR}/${NAME}_hic_matrix.h5 \
+--QCfolder ./${NAME}_hicQC
+
+echo ""
+echo "done"
