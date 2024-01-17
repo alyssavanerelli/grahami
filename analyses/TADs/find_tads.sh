014c24f7f68282a8a5bd7e24a38ca46c99a85628
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-01-17 16:32:56 -0500

    Create find_tads.sh

---
+++ analyses/TADs/find_tads.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --partition=cmain
+#SBATCH --exclude=gpuc001,gpuc002
+#SBATCH --job-name=correct_matrix
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/tads/slurmout/slurm-%j-%x.out
+#SBATCH --mem=100G
+#SBATCH -n 15
+#SBATCH -N 1
+#SBATCH --time=3-00:00:00
+#SBATCH --requeue
+#SBATCH --mail-user=av795@rutgers.edu
+#SBATCH --mail-type=FAIL
+
+echo "load conda environment"
+eval "$(conda shell.bash hook)"
+conda activate hicexplorer
+
+echo ""
+echo "load variables"
+FASTA_DIR='/projects/f_geneva_1/alyssa/grahami'
+BAM_OUTDIR="/projects/f_geneva_1/alyssa/grahami/tads/mapped_reads"
+OUTDIR='/projects/f_geneva_1/alyssa/grahami/tads/matrix'
+
+echo ""
+echo "run commands"
+hicCorrectMatrix diagnostic_plot -m ${OUTDIR}/DTG-HiC-103_hic_matrix.h5 -o ${OUTDIR}/DTG-HiC-103_hic_matrix.png
+# hicCorrectMatrix correct -m ${OUTDIR}/DTG-HiC-103_hic_matrix.h5 --filterThreshold -1.5 5 -o ${OUTDIR}/DTG-HiC-103_hic_corrected.h5
+
+echo ""
+echo "done"
