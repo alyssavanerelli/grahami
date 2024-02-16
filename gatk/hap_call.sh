8978bbbf656a28d704b9506b0e5a805e45bdc6d2
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-02-16 15:09:25 -0500

    Update hap_call.sh

--- gatk/hap_call.sh
+++ gatk/hap_call.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=p_ccib_1
-#SBATCH --exclude=gpuc001,gpuc002
+#SBATCH --partition=p_geneva_1
+#SBATCH --exclude=gpuc001,gpuc002,halc068
 #SBATCH --job-name=gatk_HC
 #SBATCH --output=/projects/f_geneva_1/alyssa/grahami/gatk/slurmout/slurm-%j-%x.out
-#SBATCH --mem=75G
+#SBATCH --mem=60G
 #SBATCH -n 2
 #SBATCH -N 1
-#SBATCH --time=14-00:00:00
+#SBATCH --time=3-16:00:00
 #SBATCH --requeue
 #SBATCH --mail-user=av795@rutgers.edu
 #SBATCH --mail-type=FAIL
@@ -23,6 +23,7 @@ module load samtools
 echo ""
 echo "load variables"
 #SAMPLE=$1
+LIST=$1
 SAMPLE='DTG-SG-149'
 INDIR="/projects/f_geneva_1/alyssa/grahami/gatk/mark_duplicates"
 GEN_DIR="/projects/f_geneva_1/alyssa/grahami"
@@ -31,7 +32,7 @@ BASE_DIR='/projects/f_geneva_1/alyssa/grahami/gatk'
 
 echo ""
 echo "run haplotype caller"
-gatk --java-options "-Xms75G -Xmx75g -XX:ParallelGCThreads=2" HaplotypeCaller --native-pair-hmm-threads 2 \
+gatk --java-options "-Xms58G -Xmx60g -XX:ParallelGCThreads=2" HaplotypeCaller --native-pair-hmm-threads 2 \
 -I ${INDIR}/${SAMPLE}.marked.bam \
 -O ${OUTDIR}/${SAMPLE}.g.vcf.gz \
 -R ${GEN_DIR}/AnoGra1.1.fa \
@@ -39,7 +40,7 @@ gatk --java-options "-Xms75G -Xmx75g -XX:ParallelGCThreads=2" HaplotypeCaller --
 --output-mode EMIT_ALL_CONFIDENT_SITES \
 --max-reads-per-alignment-start 0 \
 -RF NotDuplicateReadFilter \
--L ${BASE_DIR}/scaffold_1.list
+-L ${BASE_DIR}/${LIST}
 
 echo ""
 echo "done"
