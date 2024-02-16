688539b39e61fb984ae16096a911c7fd43a78266
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-02-16 15:13:38 -0500

    Update run_hap_call.sh

--- gatk/run_hap_call.sh
+++ gatk/run_hap_call.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LISTS=$(ls -1 /projects/f_geneva_1/alyssa/grahami/gatk/*.list | cut -d "/" -f 7 | sort | uniq)
+LISTS=$(ls -1 /projects/f_geneva_1/alyssa/grahami/gatk/*.list | cut -d "/" -f 7 | cut -d "." -f 1 | sort | uniq)
 for LIST in $LISTS
 	do
 	CMD="sbatch hap_call.sh ${LIST}"
