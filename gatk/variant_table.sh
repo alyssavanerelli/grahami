eab7e0979f50cdd9035e9cc44e4900a7d1ff565b
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-05-20 12:44:21 -0400

    Update variant_table.sh

--- gatk/variant_table.sh
+++ gatk/variant_table.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=cmain
 #SBATCH --exclude=gpuc001,gpuc002
-#SBATCH --constraint=oarc
 #SBATCH --job-name=variant_table
-#SBATCH --output=/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf/slurmout/slurm-%j-%x.out
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/gatk/slurmout/slurm-%j-%x.out
 #SBATCH --mem=10G
 #SBATCH -n 2
 #SBATCH -N 1
-#SBATCH --time=2-00:00:00
+#SBATCH --time=0-18:00:00
 #SBATCH --requeue
 #SBATCH --mail-user=av795@rutgers.edu
 #SBATCH --mail-type=FAIL
@@ -22,30 +21,34 @@ module load GATK/4.2.2.0-yc759
 
 echo ""
 echo "load variables"
-COHORT=$1
-INDIR="/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf"
-GEN_DIR="/projects/f_geneva_1/alyssa/sagrei/genome"
-OUT_SNP="/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf/snps"
-OUT_INDEL="/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf/indels"
+#SAMPLE=$1
+SAMPLE='DTG-SG-149'
+INDIR="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf"
+GEN_DIR="/projects/f_geneva_1/alyssa/grahami"
+OUTDIR="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf"
+BASE_DIR='/projects/f_geneva_1/alyssa/grahami/gatk'
+OUT_SNP="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf/snps"
+OUT_INDEL="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf/indels"
+OUT_INVARIANT="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf/invariant"
 
 
 echo ""
 echo "Variant Table SNP"
 gatk --java-options "-Xmx10g" \
 VariantsToTable \
--R ${GEN_DIR}/AnoSag2.1.fa \
--V ${OUT_SNP}/${COHORT}_snps.vcf.gz \
+-R ${GEN_DIR}/AnoGra1.1.fa \
+-V ${INDIR}/${SAMPLE}.genotype.g.vcf.gz \
 -F CHROM -F POS -F QUAL -F QD -F DP -F MQ -F MQRankSum -F FS -F ReadPosRankSum -F SOR \
--O ${OUT_SNP}/${COHORT}_snps.table
+-O ${OUT_SNP}/${SAMPLE}_snps.table
 
 echo ""
 echo "Variant Table Indel"
 gatk --java-options "-Xmx10g" \
 VariantsToTable \
--R ${GEN_DIR}/AnoSag2.1.fa \
--V ${OUT_INDEL}/${COHORT}_indels.vcf.gz \
+-R ${GEN_DIR}/AnoGra1.1.fa \
+-V ${INDIR}/${SAMPLE}.genotype.g.vcf.gz \
 -F CHROM -F POS -F QUAL -F QD -F DP -F MQ -F MQRankSum -F FS -F ReadPosRankSum -F SOR \
--O ${OUT_INDEL}/${COHORT}_indels.table
+-O ${OUT_INDEL}/${SAMPLE}_indels.table
 
 echo ""
 echo "done"
