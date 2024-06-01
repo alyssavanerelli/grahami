bb8a8c3a425a6bbb661310af21766ae195d8e5b7
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-06-01 19:18:50 -0400

    Update vcftools_filter.sh

--- gatk/vcftools_filter.sh
+++ gatk/vcftools_filter.sh
@@ -1,9 +1,8 @@
-```
 #!/bin/bash
-#SBATCH --partition=p_ccib_1
+#SBATCH --partition=cmain
 #SBATCH --exclude=gpuc001,gpuc002
 #SBATCH --job-name=vcftools_filter_snp
-#SBATCH --output=/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf/slurmout/slurm-%j-%x.out
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/gatk/slurmout/slurm-%j-%x.out
 #SBATCH --mem=10G
 #SBATCH -n 2
 #SBATCH -N 1
@@ -21,21 +20,24 @@ module load VCFtools/vcftools-v0.1.16-13-yc759
 
 echo ""
 echo "load variables"
-COHORT=$1
-INDIR="/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf"
-GEN_DIR="/projects/f_geneva_1/alyssa/sagrei/genome"
-OUT_SNP="/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf/snps"
-OUT_INDEL="/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf/indels"
-OUT_INVARIANT="/projects/f_geneva_1/alyssa/sagrei/genotype_gvcf/invariant"
-SNPS=${OUT_SNP}/${COHORT}_snps_filtered_depth_passed.vcf
+#SAMPLE=$1
+SAMPLE='DTG-SG-149'
+INDIR="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf"
+GEN_DIR="/projects/f_geneva_1/alyssa/grahami"
+OUTDIR="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf"
+BASE_DIR='/projects/f_geneva_1/alyssa/grahami/gatk'
+OUT_SNP="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf/snps"
+OUT_INDEL="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf/indels"
+OUT_INVARIANT="/projects/f_geneva_1/alyssa/grahami/gatk/genotype_gvcf/invariant"
+SNPS=${OUT_SNP}/${SAMPLE}_snps_filtered_depth_passed.vcf
 
 echo ""
 echo "Set filters for vcftools"
 MAF=0                                    # set Minor Allele Frequency
-MISS=0.9                                # set minimum missing data - Here 0.9 means we tolerate 10% missing data
+#MISS=0.9                                # set minimum missing data - Here 0.9 means we tolerate 10% missing data
 QUAL=30                                  # minimum quality score for a site to pass filtering threshold
 MIN_DEPTH=3                              # minimum mean depth and minimum depth allowed for a genotype
-MAX_DEPTH=75                           # maximum mean depth and maximum depth allowed for a genotype
+MAX_DEPTH=100                           # maximum mean depth and maximum depth allowed for a genotype
 
 echo ""
 echo "Run vcftools"
@@ -43,10 +45,11 @@ echo "Run vcftools"
 # --remove-indels                       # I left this here for just incase downstream
 
 vcftools --vcf ${SNPS} \
---maf ${MAF} --max-missing ${MISS} --minQ ${QUAL} \
+--maf ${MAF} --minQ ${QUAL} \
 --min-meanDP ${MIN_DEPTH} --max-meanDP ${MAX_DEPTH} \
---minDP ${MIN_DEPTH} --maxDP ${MAX_DEPTH} --recode --out ${OUT_SNP}/${COHORT}_snp_vcftools_filtered.vcf
-		
+--minDP ${MIN_DEPTH} --maxDP ${MAX_DEPTH} --recode --out ${OUT_SNP}/${SAMPLE}_snp_vcftools_filtered.vcf
+
+#--max-missing ${MISS}
+
 echo ""
 echo "done"
-```
