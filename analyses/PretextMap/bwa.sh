d41c96e1b744595227819db494600c1568d12526
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2025-04-09 13:46:09 -0400

    Update bwa.sh

--- analyses/PretextMap/bwa.sh
+++ analyses/PretextMap/bwa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=cmain
+#SBATCH --partition=p_ccib_1
 #SBATCH --exclude=gpuc001,gpuc002
 #SBATCH --job-name=bwa
-#SBATCH --output=/projects/f_geneva_1/alyssa/jamaica-mtgenome/phylogeny/slurmout/slurm-%j-%x.out
-#SBATCH --mem=100G
+#SBATCH --output=/projects/f_geneva_1/alyssa/grahami/pretextmap/slurmout/slurm-%j-%x.out
+#SBATCH --mem=200G
 #SBATCH -n 15
 #SBATCH -N 1
-#SBATCH --time=3-00:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --requeue
 #SBATCH --mail-user=av795@rutgers.edu
 #SBATCH --mail-type=FAIL
@@ -17,26 +17,23 @@ module load java
 module load samtools
 module load bwa
 
-
 NAME=$1
-DATA="/projects/f_geneva_1/alyssa/jamaica-mtgenome/phylogeny/data"
-INDIR="/projects/f_geneva_1/alyssa/jamaica-mtgenome/phylogeny/reads"
-OUTDIR="/projects/f_geneva_1/alyssa/jamaica-mtgenome/phylogeny/bam"
-STATS="/projects/f_geneva_1/alyssa/jamaica-mtgenome/phylogeny/bam/stats"
+INDIR="/projects/f_geneva_1/alyssa/grahami/pretextmap/files"
+OUTDIR="/projects/f_geneva_1/alyssa/grahami/pretextmap"
 
 echo ""
 echo "##################### index and align with BWA"
-#bwa index ${DATA}/grahami_mtdna.fasta
+#bwa index ${INDIR}/AnoGra1.1.fa
 
-bwa mem -t 10 ${DATA}/grahami_mtdna.fasta \
-${INDIR}/${NAME}_filtered.R1.fq.gz \
-${INDIR}/${NAME}_filtered.R2.fq.gz \
+bwa mem -t 10 ${INDIR}/AnoGra1.1.fa \
+${INDIR}/${NAME}_R1_001.fastq.gz \
+${INDIR}/${NAME}_R2_001.fastq.gz \
 | samtools sort -@10 -o ${OUTDIR}/${NAME}_bwa_aligned.bam -
 
 
 echo ""
 echo "##################### index genome with samtools - only needs to be done once"
-#samtools faidx ${DATA}/grahami_mtdna.fasta
+#samtools faidx ${INDIR}/AnoGra1.1.fa
 
 
 echo ""
