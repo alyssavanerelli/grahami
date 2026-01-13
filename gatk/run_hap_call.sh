d5cc23038b2098ea47a3bbe7e0208102b900c96a
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2026-01-13 15:47:27 -0500

    Delete gatk directory
    remove unused analyses - gatk

--- gatk/run_hap_call.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LISTS=$(ls -1 /projects/f_geneva_1/alyssa/grahami/gatk/*.list | cut -d "/" -f 7 | cut -d "." -f 1 | sort | uniq)
-for LIST in $LISTS
-	do
-	CMD="sbatch hap_call.sh ${LIST}"
-	echo $CMD
-	#eval $CMD
-	sleep 0.25
-done
--- gatk/run_mark_dup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SAMPLES=$(cut -f 1 /projects/f_geneva_1/alyssa/sagrei/4_lanes.txt | sort | uniq)
-for SAMPLE in $SAMPLES
-	do
-	CMD="sbatch mark_dup.sh ${SAMPLE}"
-	echo $CMD
-	#eval $CMD
-	sleep 0.25
-done
