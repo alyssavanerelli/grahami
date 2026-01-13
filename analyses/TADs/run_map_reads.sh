9bb7661254765ccb61f4752b8982823f1aa2b67a
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2026-01-13 16:07:25 -0500

    Delete analyses/TADs directory
    remove unused analyses - TADs

--- analyses/TADs/run_map_reads.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SAMPLES=$(ls -1 /projects/f_geneva_1/alyssa/grahami/juicerdir/AnoGra/fastq/*.gz | cut -d "/" -f 9 | cut -d "_" -f 1 | sort | uniq)
-for SAMPLE in $SAMPLES
-	do
-	CMD="sbatch map_reads.sh ${SAMPLE}"
-	echo $CMD
-	#eval $CMD
-	sleep 0.25
-done
